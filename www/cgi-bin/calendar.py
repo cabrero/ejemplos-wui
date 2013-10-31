935920e78beb969d05d3a65a09be37111f8d4925
David Cabrero <david.cabrero@madsgroup.org>
2013-10-31 13:03:01 +0100

    Cgi que únicamente devuelve la página prototipo.

---
+++ www/cgi-bin/calendar.py
@@ -0,0 +1,314 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+print """Content-Type: text/html
+
+<!doctype html>
+
+<html lang="es">
+
+  <head>
+    <meta charset="utf-8"/>
+    <title>[Calendario] Ejemplo de vista con html5+css3</title>
+    <link rel="icon" type="image/png" href="/schedule.png" />
+    <link href="/styles.css" rel="stylesheet" type="text/css" />
+  </head>
+
+  <body>
+    <header>
+      <h1>Primer cuatrimestre de 2013/2014</h1>
+    </header>
+
+    <section id="main">
+      <table class="calendar-weeks">
+	<caption>Primer cuatrimestre del curso 2013/2014</caption>
+	<thead>
+	  <tr>
+	    <th scope="col"></th>
+	    <th scope="col">Lun</th>
+	    <th scope="col">Mar</th>
+	    <th scope="col">Mié</th>
+	    <th scope="col">Jue</th>
+	    <th scope="col">Vie</th>
+	    <th scope="col">Sáb</th>
+	    <th scope="col">Dom</th>
+	  </tr>
+	</thead>
+	<tbody>
+	  <tr>
+	    <td></td>
+	    <td></td>
+	    <td></td>
+	    <td></td>
+	    <td></td>
+	    <td></td>
+	    <td></td>
+	    <td>1 Sep</td>
+	  </tr>
+	  <tr>
+	    <td></td>
+	    <td>2</td>
+	    <td>3</td>
+	    <td>4</td>
+	    <td>5</td>
+	    <td>6</td>
+	    <td>7</td>
+	    <td>8</td>
+	  </tr>
+	  <tr>
+	    <td>1</td>
+	    <td>9</td>
+	    <td>10</td>
+	    <td>11</td>
+	    <td>12</td>
+	    <td>13</td>
+	    <td>14</td>
+	    <td>15</td>
+	  </tr>
+	  <tr>
+	    <td>2</td>
+	    <td>16</td>
+	    <td>17</td>
+	    <td>18</td>
+	    <td>19</td>
+	    <td>20</td>
+	    <td>21</td>
+	    <td>22</td>
+	  </tr>
+	  <tr>
+	    <td>3</td>
+	    <td>23</td>
+	    <td>24</td>
+	    <td>25</td>
+	    <td>26</td>
+	    <td>27</td>
+	    <td>28</td>
+	    <td>29</td>
+	  </tr>
+	  <tr>
+	    <td>4</td>
+	    <td>30</td>
+	    <td>1 Oct</td>
+	    <td>2</td>
+	    <td>3</td>
+	    <td>4</td>
+	    <td>5</td>
+	    <td>6</td>
+	  </tr>
+	  <tr>
+	    <td>5</td>
+	    <td>7</td>
+	    <td>8</td>
+	    <td>9</td>
+	    <td>10</td>
+	    <td>11</td>
+	    <td>12</td>
+	    <td>13</td>
+	  </tr>
+	  <tr>
+	    <td>6</td>
+	    <td>14</td>
+	    <td>15</td>
+	    <td>16</td>
+	    <td>17</td>
+	    <td>18</td>
+	    <td>19</td>
+	    <td>20</td>
+	  </tr>
+	  <tr>
+	    <td>7</td>
+	    <td>21</td>
+	    <td>22</td>
+	    <td>23</td>
+	    <td>24</td>
+	    <td>25</td>
+	    <td>26</td>
+	    <td>27</td>
+	  </tr>
+	  <tr>
+	    <td>8</td>
+	    <td>28</td>
+	    <td>29</td>
+	    <td>30</td>
+	    <td>31</td>
+	    <td class="today">1 Nov</td>
+	    <td>2</td>
+	    <td>3</td>
+	  </tr>
+	  <tr>
+	    <td>9</td>
+	    <td>4</td>
+	    <td>5</td>
+	    <td>6</td>
+	    <td>7</td>
+	    <td>8</td>
+	    <td>9</td>
+	    <td>10</td>
+	  </tr>
+	  <tr>
+	    <td>10</td>
+	    <td>11</td>
+	    <td>12</td>
+	    <td>13</td>
+	    <td>14</td>
+	    <td>15</td>
+	    <td>16</td>
+	    <td>17</td>
+	  </tr>
+	  <tr>
+	    <td>11</td>
+	    <td>18</td>
+	    <td>19</td>
+	    <td>20</td>
+	    <td>21</td>
+	    <td>22</td>
+	    <td>23</td>
+	    <td>24</td>
+	  </tr>
+	  <tr>
+	    <td>12</td>
+	    <td>25</td>
+	    <td>26</td>
+	    <td>27</td>
+	    <td>28</td>
+	    <td>29</td>
+	    <td>30</td>
+	    <td>1 Dic</td>
+	  </tr>
+	  <tr>
+	    <td>13</td>
+	    <td>2</td>
+	    <td>3</td>
+	    <td>4</td>
+	    <td>5</td>
+	    <td>6</td>
+	    <td>7</td>
+	    <td>8</td>
+	  </tr>
+	  <tr>
+	    <td>14</td>
+	    <td>9</td>
+	    <td>10</td>
+	    <td>11</td>
+	    <td>12</td>
+	    <td>13</td>
+	    <td>14</td>
+	    <td>15</td>
+	  </tr>
+	  <tr>
+	    <td>15</td>
+	    <td>16</td>
+	    <td>17</td>
+	    <td>18</td>
+	    <td>19</td>
+	    <td>20</td>
+	    <td>21</td>
+	    <td>22</td>
+	  </tr>
+	  <tr>
+	    <td></td>
+	    <td>23</td>
+	    <td>24</td>
+	    <td>25</td>
+	    <td>26</td>
+	    <td>27</td>
+	    <td>28</td>
+	    <td>29</td>
+	  </tr>
+	  <tr>
+	    <td></td>
+	    <td>30</td>
+	    <td>31</td>
+	    <td>1 Ene</td>
+	    <td>2</td>
+	    <td>3</td>
+	    <td>4</td>
+	    <td>5</td>
+	  </tr>
+	  <tr>
+	    <td></td>
+	    <td>6</td>
+	    <td>7</td>
+	    <td>8</td>
+	    <td>9</td>
+	    <td>10</td>
+	    <td>11</td>
+	    <td>12</td>
+	  </tr>
+	  <tr>
+	    <td></td>
+	    <td>13</td>
+	    <td>14</td>
+	    <td>15</td>
+	    <td>16</td>
+	    <td>17</td>
+	    <td>18</td>
+	    <td>19</td>
+	  </tr>
+	  <tr>
+	    <td></td>
+	    <td>20</td>
+	    <td>21</td>
+	    <td>22</td>
+	    <td>23</td>
+	    <td>24</td>
+	    <td>25</td>
+	    <td>26</td>
+	  </tr>
+	  <tr>
+	    <td></td>
+	    <td>27</td>
+	    <td>28</td>
+	    <td>29</td>
+	    <td>30</td>
+	    <td>31</td>
+	    <td></td>
+	    <td></td>
+	  </tr>
+	</tbody>
+      </table>
+    </section>
+
+
+    <aside>
+      <header>
+	<h1>Contenido adicional</h1>
+      </header>
+      <ul>
+	<li><p> Lorem ipsum dolor sit amet, consectetur adipiscing
+	    elit. Morbi consectetur, felis et blandit egestas, turpis
+	    magna dapibus leo, non ullamcorper tortor justo vel
+	    turpis. Nullam id sagittis libero, eu facilisis
+	    felis. Duis a faucibus libero. Vestibulum pharetra id
+	    tellus vitae fermentum. Quisque ut pulvinar enim. Nam
+	    auctor molestie imperdiet. Nulla cursus justo eu tempor
+	    accumsan. Donec euismod a turpis vel adipiscing. Morbi
+	    commodo ante nec feugiat fringilla. Pellentesque facilisis
+	    purus eu magna viverra consequat. Nulla tortor dui,
+	    commodo vitae ipsum tincidunt, consectetur blandit
+	    metus. Donec eu dui euismod, molestie eros et, euismod
+	    purus. Fusce tellus felis, ullamcorper non justo sed,
+	    porttitor accumsan eros. Pellentesque habitant morbi
+	    tristique senectus et netus et malesuada fames ac turpis
+	    egestas.
+	</p></li>
+	<li><p> In viverra, ante ac placerat sollicitudin, ante diam
+	    laoreet justo, suscipit rhoncus metus turpis a
+	    magna. Interdum et malesuada fames ac ante ipsum primis in
+	    faucibus. Nulla facilisi. Duis accumsan est vel congue
+	    consectetur. Praesent pellentesque euismod massa, sed
+	    scelerisque justo consectetur suscipit. Nunc a mollis
+	    arcu, eu eleifend justo. Proin sodales lectus sed turpis
+	    commodo, eget adipiscing arcu consectetur. Curabitur et
+	    augue in urna porta condimentum sit amet in lorem. Proin
+	    tincidunt velit neque, bibendum euismod mi semper sit
+	    amet. In pulvinar leo volutpat turpis auctor vestibulum.
+	</p></li>
+      </ul>
+    </aside>
+
+  </body>
+
+</html>
+"""
